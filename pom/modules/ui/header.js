be07facf1f1298c5b013d9eb56a80aa61bde8051
mldvnv <milan.moldovanov@yahoo.com>
2025-02-12 10:36:03 +0100

    Added methods

--- pom/modules/ui/header.js
+++ pom/modules/ui/header.js
@@ -1,3 +1,7 @@
+import { LoginPage } from "../../pom/modules/ui/loginPage";
+import { LoginPage } from "../../pom/modules/ui/loginPage";
+import { RegisterPage } from "./registerPage";
+
 export class Header {
   constructor(page) {
     this.page = page;
@@ -6,6 +10,37 @@ export class Header {
     );
     this.cogwheel = page.locator(".md:ml-4 sm:ml-2  h-14 w-14 py-1");
     this.registerPageRedirect = page.locator('text ="Register"');
-    this.loginPageRedirect = page.locator('text = "Log in');
+    this.loginPageRedirect = page.locator('text = "Log in"');
+  }
+
+  async appeareanceChangeLogin(page) {
+    const loginPage = new LoginPage();
+    loginPage.login(
+      VALID_LOGIN_PAYLOAD["EMAIL"],
+      VALID_LOGIN_PAYLOAD["PASSWORD"]
+    );
+    await page.registerPageRedirect.toBeHidden();
+    await page.loginPageRedirect.toBeHidden();
+
+    this.cart = page.locator(
+      ".inline-flex items-center py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-primary hover:text-gray-600 focus:outline-none transition ease-in-out duration-150 p-button p-component"
+    );
+    this.dropDown = page.locator(
+      ".inline-flex items-center py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-primary hover:text-gray-600 focus:outline-none transition ease-in-out duration-150 p-button p-component"
+    );
+  }
+
+  async appeareanceChangeLogin(page) {
+    const registerPage = new RegisterPage();
+    await registerPage.register(username, email, password);
+    await page.registerPageRedirect.toBeHidden();
+    await page.loginPageRedirect.toBeHidden();
+
+    this.cart = page.locator(
+      ".inline-flex items-center py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-primary hover:text-gray-600 focus:outline-none transition ease-in-out duration-150 p-button p-component"
+    );
+    this.dropDown = page.locator(
+      ".inline-flex items-center py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-primary hover:text-gray-600 focus:outline-none transition ease-in-out duration-150 p-button p-component"
+    );
   }
 }
