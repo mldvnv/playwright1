10c15eaa7069e4b34655586908905070b5f7d973
mldvnv <milan.moldovanov@yahoo.com>
2025-02-13 19:53:46 +0100

    Altered locators

--- pom/modules/ui/dashboard.js
+++ pom/modules/ui/dashboard.js
@@ -2,29 +2,15 @@ export class Dashboard {
   constructor(page) {
     this.page = page;
     this.searchBar = page.locator("flex flex-col >> .search");
-    this.applyFilter = page.locator(
-      'layout-menu sm:w-48 md:w-72 bg-gray-200 rounded h-max >> text ="Apply filters"'
-    );
-    this.deselectFilter = page.locator(
-      'layout-menu sm:w-48 md:w-72 bg-gray-200 rounded h-max >> text="Deselect all'
-    );
-    this.gpu = page.locator('layout-root-menuitem >> text="GPUs" ');
-    this.cpu = page.locator('layout-root-menuitem >> text="CPUs"');
-    this.laptopPC = page.locator(
-      'layout-root-menuitem >> text="Laptops & PCs"'
-    );
-    this.phoneTables = page.locator(
-      'layout-root-menuitem >> text="Phones & Tablets"'
-    );
-    this.peripherals = page.locator(
-      'layout-root-menuitem >> text="Peripherals"'
-    );
-    this.computerCase = page.locator(
-      'layout-root-menuitem >> text="Computer Cases"'
-    );
-    this.motherBoard = page.locator(
-      'layout-root-menuitem >> text = "Motherboards"'
-    );
+    this.applyFilter = page.locator(' text ="Apply filters"');
+    this.deselectFilter = page.locator(' text="Deselect all');
+    this.gpu = page.locator(' text="GPUs" ');
+    this.cpu = page.locator(' text="CPUs"');
+    this.laptopPC = page.locator(' text="Laptops & PCs"');
+    this.phoneTables = page.locator(' text="Phones & Tablets"');
+    this.peripherals = page.locator(' text="Peripherals"');
+    this.computerCase = page.locator('text="Computer Cases"');
+    this.motherBoard = page.locator('text = "Motherboards"');
     this.products = page.locator("#products-container");
     this.pageOne = page.locator(
       "paginated w-full flex justify-center > text=1"
