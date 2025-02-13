fbdb94cf0926a3bd6a0b830e1979c8c02c99e215
mldvnv <milan.moldovanov@yahoo.com>
2025-02-13 20:03:09 +0100

    Assigned variables to URLs

--- fixtures/urls.js
+++ fixtures/urls.js
@@ -2,4 +2,8 @@ export const URLS = {
   LOGIN: "/login",
   REGISTER: "/register",
   DASHBOARD: "/dashboard",
+  Automaticity: "https://www.automaticity.rs/",
+  LinkedIn: "https://www.linkedin.com/company/automaticity-it/mycompany/",
+  Facebook: "https://www.facebook.com/automaticity.qa",
+  Instagram: "https://www.instagram.com/automaticity.qa/",
 };
